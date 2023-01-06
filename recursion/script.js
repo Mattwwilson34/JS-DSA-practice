f36f90b4520f1005e4c894d5dbc62d1af0293203
Matt Wilson <mattwwilson34@gmail.com>
2023-01-06 10:50:40 -0500

    Add: countItems function

--- recursion/script.js
+++ recursion/script.js
@@ -11,4 +11,12 @@ const sum = (array) => {
   return array[0] + sum(array.slice(1));
 };
 
-console.log(sum([2, 4, 5]));
+// console.log(sum([2, 4, 5]));
+
+const countItems = (array) => {
+  if (array.length <= 0) return 0;
+  console.log(array);
+  return 1 + countItems(array.slice(1));
+};
+
+console.log(countItems([1, 4, 6]));

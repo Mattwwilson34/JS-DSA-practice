1411caac1aca0ce402d61daaf2ad1c3a74a455cf
Matt Wilson <mattwwilson34@gmail.com>
2023-01-26 11:45:26 -0500

    Add: append function to linked list class

--- linked-list/script.js
+++ linked-list/script.js
@@ -1,12 +1,42 @@
 "use strict";
 class ListNode {
-    constructor(data) {
-        this.data = data;
+    constructor(val = null) {
         this.next = null;
-        this.prev = null;
+        this.val = val;
     }
 }
-let node1 = new ListNode('node 1 data');
-let node2 = new ListNode('node2 data');
-node1.next = node2;
-console.log(node1);
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+    append(value = 0) {
+        // handle empty list
+        if (!this.head) {
+            this.head = new ListNode(value);
+        }
+        let currentNode = this.head;
+        while (currentNode.next !== null) {
+            currentNode = currentNode.next;
+        }
+        let newNode = new ListNode(value);
+        currentNode.next = newNode;
+    }
+}
+const head = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+const node4 = new ListNode(4);
+const node5 = new ListNode(5);
+const node6 = new ListNode(6);
+const node7 = new ListNode(7);
+head.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+node5.next = node6;
+node6.next = node7;
+const list = new LinkedList(head);
+list.head = head;
+const list2 = new LinkedList();
+list.append(100);
+console.log(list);
--- linked-list/script.ts
+++ linked-list/script.ts
@@ -1,15 +1,60 @@
-class ListNode<T> {
-	public next: ListNode<T> | null;
+class ListNode<Type> {
+	public next: ListNode<Type> | null;
+	public val: number | null;
 
-	constructor(val?: T) {
+	constructor(val: number | null = null) {
 		this.next = null;
-		this.? val = val;
+		this.val = val;
 	}
 }
 
-let node1 = new ListNode(12)
-let node2 = new ListNode('node2 data')
+class LinkedList<Type> {
+	public head: ListNode<Type> | null;
+
+	constructor(head: ListNode<Type> | null = null) {
+		this.head = head;
+	}
+
+	public append(value: number = 0): void {
+		// handle empty list
+		if (!this.head) {
+			this.head = new ListNode(value)
+		}
+
+		let currentNode = this.head;
+
+		// move to end of linked list
+		while (currentNode.next !== null) {
+			currentNode = currentNode.next
+		}
+		// create new node and add to tail of list
+		let newNode = new ListNode(value)
+		currentNode.next = newNode
+	}
+}
 
 
-// Review generic types and see if you can create a linked list that
-// can accept any tupe as the data value.
+const head = new ListNode(1)
+const node2 = new ListNode(2)
+const node3 = new ListNode(3)
+const node4 = new ListNode(4)
+const node5 = new ListNode(5)
+const node6 = new ListNode(6)
+const node7 = new ListNode(7)
+
+head.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+
+const list = new LinkedList(head)
+
+list.head = head;
+
+const list2 = new LinkedList()
+
+list.append(100)
+console.log(list)
+
